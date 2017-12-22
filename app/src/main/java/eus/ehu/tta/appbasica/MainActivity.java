016d69c093bfee1ba7f1f0dff9771d8badcc3aea
Gonzalo Gil <gonzalog26gil@gmail.com>
2017-12-22 16:43:25 +0100

    Actividades, layouts y alguna transicion entre paginas creados

--- app/src/main/java/eus/ehu/tta/appbasica/MainActivity.java
+++ app/src/main/java/eus/ehu/tta/appbasica/MainActivity.java
@@ -1,7 +1,10 @@
 package eus.ehu.tta.appbasica;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void login (View view){
+
+        Intent intent = new Intent(this,MenuActivity.class);
+        String login = ((EditText)findViewById(R.id.login)).getText().toString();
+        String passwd = ((EditText)findViewById(R.id.passwd)).getText().toString();
+        startActivity(intent);
     }
 }
