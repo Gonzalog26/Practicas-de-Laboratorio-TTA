14a777918694329d248f8b5d206f97785fd0b9bd
Gonzalo Gil <gonzalog26gil@gmail.com>
2017-12-27 09:27:58 +0100

    actividades Main, Menu y Ejercicio terminadas

--- app/src/main/java/eus/ehu/tta/appbasica/EjercicioActivity.java
+++ app/src/main/java/eus/ehu/tta/appbasica/EjercicioActivity.java
@@ -2,6 +2,8 @@ package eus.ehu.tta.appbasica;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class EjercicioActivity extends AppCompatActivity {
 
@@ -9,5 +11,24 @@ public class EjercicioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ejercicio);
+
+        TextView textEjercicio = (TextView)findViewById(R.id.enunciado_ejercicio);
+        textEjercicio.setText(R.string.ejercicio1);
+    }
+
+    public void enviarFichero(View view){
+
+    }
+
+    public void sacarFoto(View view){
+
+    }
+
+    public void grabarAudio(View view){
+
+    }
+
+    public void grabarVideo(View view){
+
     }
 }
--- app/src/main/java/eus/ehu/tta/appbasica/MenuActivity.java
+++ app/src/main/java/eus/ehu/tta/appbasica/MenuActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -13,17 +15,24 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        Intent intent = getIntent();
+        TextView textLogin = (TextView) findViewById(R.id.menu_login);
+        String text = R.string.bienvenido+intent.getStringExtra(EXTRA_LOGIN);
+        textLogin.setText(text);
+        textLogin = (TextView)findViewById(R.id.leccion);
+        textLogin.setText(R.string.leccion1);
     }
 
-   public void test (View view){
-       Intent intent = new Intent(this,testsActivity.class);
-       startActivity(intent);
-   }
+    public void test (View view){
+        Intent intent = new Intent(this,testsActivity.class);
+        startActivity(intent);
+    }
 
-   public void ejercicio (View view){
-       Intent intent = new Intent(this, EjercicioActivity.class);
-       startActivity(intent);
-   }
+    public void ejercicio (View view){
+        Intent intent = new Intent(this, EjercicioActivity.class);
+        startActivity(intent);
+    }
 
 
 }
