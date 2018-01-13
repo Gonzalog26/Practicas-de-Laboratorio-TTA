2823e8ad4020b0c1be65fe2a02ee67712d4d7dfa
Gonzalo Gil <gonzalog26gil@gmail.com>
2018-01-13 14:53:23 +0100

    Practica 2 terminada

--- app/src/main/java/eus/ehu/tta/appbasica/EjercicioActivity.java
+++ app/src/main/java/eus/ehu/tta/appbasica/EjercicioActivity.java
@@ -1,11 +1,14 @@
 package eus.ehu.tta.appbasica;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.provider.OpenableColumns;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -20,11 +23,11 @@ import java.io.IOException;
 
 public class EjercicioActivity extends AppCompatActivity {
 
-    private final int WRITE_PERMISSION_CODE = 1;
-    private final int AUDIO_REQUEST_CODE = 1;
-    private final int VIDEO_REQUEST_CODE = 2;
-    private final int PICTURE_REQUEST_CODE = 3;
+    private final int PICTURE_REQUEST_CODE = 1;
+    private final int AUDIO_REQUEST_CODE = 2;
+    private final int VIDEO_REQUEST_CODE = 3;
     private final int READ_REQUEST_CODE = 4;
+    private final int WRITE_PERMISSION_CODE = 5;
 
     Uri pictureUri;
 
@@ -39,6 +42,32 @@ public class EjercicioActivity extends AppCompatActivity {
 
     public void enviarFichero(View view){
 
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("*/*");
+        startActivityForResult(intent, READ_REQUEST_CODE);
+    }
+
+    private void dumpMetadata(Uri uri) {
+
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null, null);
+
+        try {
+            if (cursor != null && cursor.moveToFirst()) {
+                String displayName = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+
+                int sizeIndex = cursor.getColumnIndex(OpenableColumns.SIZE);
+                String size = null;
+                if (!cursor.isNull(sizeIndex))
+                    size = cursor.getString(sizeIndex);
+                else
+                    size = "Unknown";
+
+                Toast.makeText(this, "Nombre: "+displayName+" Tamaño: "+size, Toast.LENGTH_SHORT).show();
+            }
+        } finally {
+            cursor.close();
+        }
     }
 
     public void sacarFoto(View view){
@@ -70,7 +99,7 @@ public class EjercicioActivity extends AppCompatActivity {
                 try {
 
                     File file = File.createTempFile("tta", ".jpg", dir);
-                    Uri pictureUri = Uri.fromFile(file);
+                    pictureUri = Uri.fromFile(file);
                     intent.putExtra(MediaStore.EXTRA_OUTPUT, pictureUri);
                     startActivityForResult(intent, PICTURE_REQUEST_CODE);
                 } catch (IOException ex) {
@@ -91,7 +120,7 @@ public class EjercicioActivity extends AppCompatActivity {
             if (intent.resolveActivity(getPackageManager()) != null) {
                 startActivityForResult(intent, AUDIO_REQUEST_CODE);
             } else
-                Toast.makeText(this, R.string.noapp, Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, R.string.noappgrabador, Toast.LENGTH_SHORT).show();
 
         }
     }
@@ -104,9 +133,35 @@ public class EjercicioActivity extends AppCompatActivity {
             if (intent.resolveActivity(getPackageManager()) != null) {
                 startActivityForResult(intent, VIDEO_REQUEST_CODE);
             } else
-                Toast.makeText(this, R.string.noapp, Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, R.string.noappgrabadorvideo, Toast.LENGTH_SHORT).show();
 
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (resultCode != Activity.RESULT_OK)
+            return;
+
+        switch (requestCode) {
+            case READ_REQUEST_CODE:
+                dumpMetadata(data.getData());
+                sendFile(data.getData());
+                break;
+            case VIDEO_REQUEST_CODE:
+                sendFile(data.getData());
+            case AUDIO_REQUEST_CODE:
+                sendFile(data.getData());
+                break;
+            case PICTURE_REQUEST_CODE:
+                sendFile(pictureUri);
+                break;
+        }
+    }
+
+    public void sendFile(Uri uri){
+        Toast.makeText(this, uri.toString(), Toast.LENGTH_SHORT).show();
     }
 
 }
